9aa1a6899ca2b260adf9eaae593d78986dc1dff3
Lucas Oliveira <purzbbq@gmail.com>
2021-09-10 15:51:54 +0200

    fetch data & return on index html

--- orinoco-frontend/js/index.js
+++ orinoco-frontend/js/index.js
@@ -3,9 +3,6 @@ const productsList = document.getElementById("productsList");
 const url = "http://localhost:3000/api/cameras";
 
 /* Function */
-function createElement(element){
-    return element.createElement(element);
-};
 
 function append(parent,el){
     return parent.appendChild(el);
@@ -16,7 +13,35 @@ fetch(url)
 return res.json()
   })
   .then(function(data){
-      document.getElementById("productsList").innerHTML = data[0].name;
+      for (let i = 0; i < data.length; i++){
+          let col = document.createElement('div');
+          let card = document.createElement('div');
+          let cardBody = document.createElement('div');
+          let cardTitle = document.createElement('h5');
+          let cardText = document.createElement('p');
+          let img = document.createElement('img');
+          let cardBtn = document.createElement('a');
+          img.setAttribute('src',data[i].imageUrl);
+          cardBtn.setAttribute('href',"produit.html");
+          col.className = "col12 col-lg-4 d-flex align-items-stretch";
+          card.className = ("card border-muted shadow text-center mb-4");
+          cardBody.className = ("card-body");
+          img.className = ("card-img-top");
+          cardTitle.className = ("card-title");
+          cardText.className = ("card-text");
+          cardBtn.className = ("btn btn-outline-dark");
+          cardBtn.innerText = "Voir le produit";
+          cardText.innerHTML = data[i].description;
+          cardTitle.innerHTML = data[i].name;
+          append(productsList,col);
+          append(col,card);
+          append(card,img);
+          append(card,cardBody);
+          append(cardBody,cardTitle);
+          append(cardBody,cardText);
+          append(cardBody,cardBtn);
+          
+      } 
   })
 .catch(function(error) {
  document.getElementById("productsList").innerHTML = "<h3>erreur</h3>"
