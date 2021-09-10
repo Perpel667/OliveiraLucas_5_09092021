4959ce37eff26d765bb280f29fe30a96184b080d
Lucas Oliveira <purzbbq@gmail.com>
2021-09-10 14:26:59 +0200

    1st fetch valid

--- orinoco-frontend/js/index.js
+++ orinoco-frontend/js/index.js
@@ -1,6 +1,28 @@
-const camURL = "http://localhost:3000/api/cameras";
+/* Variables */
+const productsList = document.getElementById("productsList");
+const url = "http://localhost:3000/api/cameras";
 
- fetch(camURL)
-  .then(response => response.json())
-  .then(data => console.log(data));
+/* Function */
+function createElement(element){
+    return element.createElement(element);
+};
+
+function append(parent,el){
+    return parent.appendChild(el);
+}
+
+fetch(url)
+.then(function(res) {
+return res.json()
+  })
+  .then(function(data){
+      document.getElementById("productsList").innerHTML = data[0].name;
+  })
+.catch(function(error) {
+ document.getElementById("productsList").innerHTML = "<h3>erreur</h3>"
+});
+  
+
+
+
 
