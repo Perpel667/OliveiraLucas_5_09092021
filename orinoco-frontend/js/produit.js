c9c08016d21a7d3660091aeca147144504fab68c
Lucas Oliveira <purzbbq@gmail.com>
2021-09-16 12:07:31 +0200

    removed confirm addCart for a html modal

--- orinoco-frontend/js/produit.js
+++ orinoco-frontend/js/produit.js
@@ -21,32 +21,6 @@ function removeClasses() {
 }
 /* Structure de la carte produit */
 
-/* confirmation panier */
-let confirmAdd = false;
-
-const confirmAddCart = () => {
-  let successAlert = document.createElement('p');
-        append(addToCart,successAlert);
-        successAlert.innerText = "Ajouté ✔️";
-        successAlert.className = "my-4 ms-2 text-success"
-        hide();
-        confirmAdd = true;
-
-//cache le texte de confirmation au bout de 3sec //
-function hide() {
-  timeoutID = window.setTimeout(hideClass, 3000);
-}
-function hideClass() {
-  successAlert.className = "d-none";
-}
-
-}
-// Empeche de montrer Ajouté ✔️ a chaque clic //
-const confirmAddCartOnce = () =>{
-  if (!confirmAdd){
-    confirmAddCart();
-  }
-}
 
 /*************/
 /* Fonctions */
