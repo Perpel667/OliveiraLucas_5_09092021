0424d7fb60c9fe502bb09caccc0cecf03f803995
Lucas Oliveira <purzbbq@gmail.com>
2021-09-20 16:26:26 +0200

    starting JS on confirm page

---
+++ orinoco-frontend/js/confirm.js
@@ -0,0 +1,35 @@
+///////////////////////////////////////////
+ /*Variables*/
+///////////////////////////////////////////
+let params = new URL(document.location).searchParams;
+let id = params.get('orderId');
+///////////////////////////////////////////
+ /*recuperation des produits selectionnés */
+///////////////////////////////////////////
+let produits = JSON.parse(localStorage.getItem('produits'));
+
+/*************/
+/* Functions */
+/*************/
+function calculTotal(){
+    toutLesPrix = [];
+   for (let j = 0; j < produits.length; j++){
+       toutLesPrix.push(produits[j].prix);
+   }
+    prixTotal = toutLesPrix.reduce(function(a, b){
+       return a + b;
+   },0);
+   return prixTotal;
+}
+/* Affichage du message de la confirmation */
+calculTotal();
+main = document.getElementById('main');
+main.innerHTML = `<div class="container vh-100 d-flex align-items-center justify-content-center">
+<div class="card">
+<div class="card-body text-center">
+  <h5 class="card-title text-success">Achat confirmé</h5>
+  <p class="card-text">Merci de nous avoir fait confiance pour votre achat d'un montant de <strong>${prixTotal}€</strong>.</p>
+  <p class="card-text">Voici votre numero de commande : <strong>${id}</strong>.</p>
+</div>
+</div>
+                    </div>`;
