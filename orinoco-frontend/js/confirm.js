351cc45eecc802dde1d914fa81320143e7b6c966
Lucas Oliveira <purzbbq@gmail.com>
2021-09-20 16:44:17 +0200

    remove items when we click on return link

--- orinoco-frontend/js/confirm.js
+++ orinoco-frontend/js/confirm.js
@@ -11,6 +11,7 @@ let produits = JSON.parse(localStorage.getItem('produits'));
 /*************/
 /* Functions */
 /*************/
+// calcul du prix total //
 function calculTotal(){
     toutLesPrix = [];
    for (let j = 0; j < produits.length; j++){
@@ -21,6 +22,10 @@ function calculTotal(){
    },0);
    return prixTotal;
 }
+// vide le localStorage //
+function removeItems(){
+    localStorage.removeItem('produits');
+}
 /* Affichage du message de la confirmation */
 calculTotal();
 main = document.getElementById('main');
@@ -30,6 +35,13 @@ main.innerHTML = `<div class="container vh-100 d-flex align-items-center justify
   <h5 class="card-title text-success">Achat confirmé</h5>
   <p class="card-text">Merci de nous avoir fait confiance pour votre achat d'un montant de <strong>${prixTotal}€</strong>.</p>
   <p class="card-text">Voici votre numero de commande : <strong>${id}</strong>.</p>
+  <a class="text-secondary" href="index.html" id="returnLink"><i class="bi bi-arrow-left-short"></i>Retour vers l'acceuil<a></a>
 </div>
 </div>
                     </div>`;
+
+// vide le localStorage au clic //
+returnLink = document.getElementById('returnLink');
+returnLink.addEventListener('click',function(e){
+    removeItems();
+})
